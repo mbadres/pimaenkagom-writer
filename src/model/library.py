1a58e6a92a7b8a92d55ec9ec5747d6de012e944b
Miena Basta Badres <miena.bastabadres@protonmail.com>
2022-01-01 17:24:12 +0100

    Add some getter to the library element class.

--- src/model/library.py
+++ src/model/library.py
@@ -43,6 +43,55 @@ class Element:
 		"""
 		return self.__titled
 
+	def type(self):
+		"""
+		This method returns the element type.
+		:return: A string
+		"""
+		return self.__type
+
+	def node_type(self):
+		"""
+		This method returns the element node type.
+		:return: A string
+		"""
+		return self.__node_type
+
+	def audible(self):
+		"""
+		This method returns true if the element is set audible.
+		:return: A boolean
+		"""
+		return self.__audible
+
+	def visible(self):
+		"""
+		This method returns true if the element is set visible.
+		:return: A boolean
+		"""
+		return self.__visible
+
+	def icon(self):
+		"""
+		This method returns the icon.
+		:return: A string
+		"""
+		return self.__icon
+
+	def version(self):
+		"""
+		This method returns the version.
+		:return: An integer
+		"""
+		return self.__version
+
+	def children(self):
+		"""
+		This method returns the children.
+		:return: A list of lists
+		"""
+		return self.__children
+
 
 class Library:
 	"""
