f2e0458fc8a2d438b9bd99f96d5bba00934c15fe
Miena Basta Badres <miena.bastabadres@protonmail.com>
2022-01-22 03:02:08 +0100

    Add other generator methods.

--- src/tools/generation.py
+++ src/tools/generation.py
@@ -1,7 +1,8 @@
 import uuid
+from random import randint, choice
 
 from model.library import Library
-from model.multilingual_string import String
+from multilingual.multilingual_string import String
 
 
 class Generator:
@@ -17,9 +18,61 @@ class Generator:
 	@staticmethod
 	def generate_id() -> int:
 		"""
-		This method generates a random id.
+		This static method generates a random id.
 		:return: A 64-bit integer
 		"""
 		maxint64 = (1 << 64) - 1
 		randint128 = uuid.uuid4().int
 		return maxint64 & randint128
+
+	@staticmethod
+	def generate_words(consonants: [str] = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"],
+	                   vowel: [str] = ["a", "e", "i", "o", "u"]) -> str:
+		"""
+		This static method generates a random word.
+		:param consonants: A list of strings
+		:param vowel: A list of strings
+		:return: A string.
+		"""
+
+		# set constants for the randomisation engine
+		min_number_letters: int = 1
+		max_number_letters: int = 14
+
+		# determine the word length
+		length = randint(min_number_letters, max_number_letters)
+
+		# store the result
+		word = ""
+
+		# construct the word
+		for _ in range(length):
+			word += choice(consonants)
+			word += choice(vowel)
+
+		return word
+
+	@staticmethod
+	def generate_string() -> String:
+		"""
+		TODO implement
+		This static method generates a random multilingual string.
+		:return: A multilingual string.
+		"""
+
+		# set constants for the randomisation engine
+		min_number_words: int = 3
+		max_number_words: int = 14
+
+
+
+		return String(empty=True)
+
+	@staticmethod
+	def generate_library() -> Library:
+		"""
+		TODO implement
+		This static method generates a random library.
+		:return: A library.
+		"""
+		return Library()
