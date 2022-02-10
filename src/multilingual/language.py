07407cb44ff7776117adea843e6f025d5f541a46
Miena Basta Badres <miena.bastabadres@protonmail.com>
2022-02-10 23:06:43 +0100

    Add an enum for languages.

---
+++ src/multilingual/language.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Language(Enum):
+	"""
+	This class enumerates the languages of a multilingual string.
+	"""
+
+	HEBREW = 0
+	ANCIENT_GREEK = 1
+	SYRIAC = 2
+	COPTIC = 3
+	ARABIC = 4
+	ENGLISH = 5
+	GERMAN = 6
